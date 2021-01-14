01509202a7ea0805f4678ce98ee6fe0db56f6407
helman101 <helmanandresortegon@gmail.com>
2021-01-14 15:57:30 +0100

    edited show and create actions and templates to display the blog input

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,22 @@
 class ArticlesController < ApplicationController
-    def new
-    end
+  def new
+  end
 
-    def create
-      render plain: params[:article].inspect
-    end
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def create
+    #render plain: params[:article].inspect
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to @article
+  end
+
+private
+
+  def article_params
+    params.require(:article).permit(:title, :text)
+  end
+  
 end
